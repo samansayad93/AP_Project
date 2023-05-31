ae328b4dffbeeb13a5debcbf23c7be756c09b267
Saman Sayad <samansayad93@gmail.com>
2023-05-31 12:16:48 +0330

    FrmSignUp INIT

---
+++ Project/FrmLogin.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for FrmLogin.xaml
+    /// </summary>
+    public partial class FrmLogin : Window
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            FrmSignUp frmSignUp = new FrmSignUp();
+            frmSignUp.ShowDialog();
+        }
+    }
+}
